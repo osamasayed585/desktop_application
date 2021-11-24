e49522565ad58c59aae6fa1a6cefc4d4cfc0ce29
Osama Sayed <osamasayed151@gmail.com>
2021-11-24 03:53:32 +0200

    add new features

--- FirstApp/Form1.cs
+++ FirstApp/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+using System.Data.OleDb;
 
 namespace FirstApp
 {
@@ -38,78 +41,120 @@ namespace FirstApp
 
         }
 
+        private void clearFields()
+        {
+            txtID.Text = "";
+            txtName.Text = "";
+            txtPhone.Text = "";
+            txtAddress.Text = "";
+            NUDAge.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection CONN = new SqlConnection("Data Source=OS;Initial Catalog=FirstDatabase;Integrated Security=True");
+
+            SqlCommand cmmd = new SqlCommand();
+
+            cmmd.Connection = CONN;
+            cmmd.CommandText = "select * from Students where id="+txtID.Text+"";
+            clearFields();
+
+            CONN.Open();
+
+            DataTable table = new DataTable();
+            table.Load(cmmd.ExecuteReader());
+
+            txtID.Text = Convert.ToString(table.Rows[0][0]);
+            txtName.Text = table.Rows[0][1].ToString();
+            txtPhone.Text = table.Rows[0][2].ToString();
+            txtAddress.Text = table.Rows[0][3].ToString();
+            NUDAge.Value = Convert.ToDecimal(table.Rows[0][4]);
+   
+            MessageBox.Show("تم جلب البيانات بنجاح");
+            CONN.Close();
+
+        }
+
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            SqlConnection CONN = new SqlConnection("Data Source=OS;Initial Catalog=FirstDatabase;Integrated Security=True");
+
+            SqlCommand cmmd = new SqlCommand();
+
+            cmmd.Connection = CONN;
+            cmmd.CommandText = "select * from Students where id=" + txtID.Text + "";
+            clearFields();
+
+            CONN.Open();
+
+            DataTable table = new DataTable();
+            table.Load(cmmd.ExecuteReader());
+
+            txtID.Text = Convert.ToString(table.Rows[0][0]);
+            txtName.Text = table.Rows[0][1].ToString();
+            txtPhone.Text = table.Rows[0][2].ToString();
+            txtAddress.Text = table.Rows[0][3].ToString();
+            NUDAge.Value = Convert.ToDecimal(table.Rows[0][4]);
+
+            MessageBox.Show("تم جلب البيانات بنجاح");
+            CONN.Close();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+         
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            SqlConnection CONN = new SqlConnection("Data Source=OS;Initial Catalog=FirstDatabase;Integrated Security=True");
+
+            SqlCommand cmmd = new SqlCommand();
+
+            cmmd.Connection = CONN;
+            cmmd.CommandText = "select id as 'كود الطالب' , name as 'اسم الطالب', phone as 'رقم تليفون الطالب', address as 'العنوان' , age as 'العمر' from Students";
+            clearFields();
+
+            CONN.Open();
+
+            DataTable table = new DataTable();
+            table.Load(cmmd.ExecuteReader());
+
+            dataGridView1.DataSource = table;
+
+            CONN.Close();
+        }
+
         private void btn_add_Click(object sender, EventArgs e)
         {
+            /*   SqlConnection CONN = new SqlConnection("Data Source=OS;Initial Catalog=FirstDatabase;Integrated Security=True");
 
-          //  num1 = Convert.ToInt32(txtBox1.Text);
+              SqlCommand cmmd = new SqlCommand();
 
-             for (int i = 0; i< 10; i++){
-                MessageBox.Show("value of "+i+" from 10");
-            } 
-            
-          
-            
-            /*   switch (num1)
-            {
-                case 30: {
-                        MessageBox.Show("اجابة خاطئه");
-                        break;
-                    }
-                case 40:
-                    {
-                        MessageBox.Show("اجابة خاطئه");
-                        break;
-                    }
-                case 50:
-                    {
-                        MessageBox.Show("اجابة صحيحه");
-                        break;
-                    }
-                case 70:
-                    {
-                        MessageBox.Show("اجابة خاطئه");
-                        break;
-                    }
-                default: 
-                    {
-                        MessageBox.Show("اجابة خاطئه");
-                        break;
-                    } */
-            
-        
+              cmmd.Connection = CONN;
+              cmmd.CommandText = "insert into Students values ("+txtID.Text+" , '"+txtName.Text+ "' , '"+txtPhone.Text+ "' , '"+txtAddress.Text+ "' , "+NUDAge.Text+ ")";
+              clearFields();
 
-            /* if ( num1  >= 50 && num1 < 60){
+              CONN.Open();
+              cmmd.ExecuteNonQuery();
+              MessageBox.Show("تم الحفظ بنجاح");
+              CONN.Close(); */
 
-                 MessageBox.Show("انت تقديرك مقبول", "مبروك", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\FirstDB.accdb");
+            OleDbCommand cmmd = new OleDbCommand();
 
-             }
-             else if (num1 >= 60 && num1 < 70)
-             {
-                 MessageBox.Show("انت تقديرك جيد", "مبروك", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            cmmd.Connection = conn;
+            cmmd.CommandText = "insert into Students values (" + txtID.Text + " , '" + txtName.Text + "' , '" + txtPhone.Text + "' , '" + txtAddress.Text + "' , " + NUDAge.Text + ")";
 
-             }
-             else if (num1 >= 70 && num1 < 80)
-             {
-                 MessageBox.Show("انت تقديرك جيد جدا", "مبروك", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            clearFields();
+            conn.Open();
 
-             }
-             else if (num1 >= 80 && num1 < 90)
-             {
-                 MessageBox.Show("انت تقديرك ممتاز", "مبروك", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            cmmd.ExecuteNonQuery();
+            MessageBox.Show("تم حفظ البيانات ", "تأكيد");
 
-             }
-             else if (num1 >= 90 )
-             {
-                 MessageBox.Show("انت تقديرك ممتاز مع مرتبة الشرف", "مبروك", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-             }
-             else{
-                 MessageBox.Show("انت لم تنج يا فاشل ", "حاول مره اخري", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-             } */
-
-
-
+            conn.Close();
 
 
         }
